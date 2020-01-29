ebccd7bed1a4ec285f3fde0c8b3031b9ffd1e958
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-01-29 15:21:37 +0530

    Update DiffPrivacy.jl

--- src/DiffPrivacy.jl
+++ src/DiffPrivacy.jl
@@ -1,1 +1,15 @@
+include("get_one_parallel_database.jl")
 
+function get_multiple_parallel_databases(A)
+
+    # Returns Array of length(A) with all the parallel databases generated and stored in parallel_databases
+
+    parallel_databases = []
+    
+    for i in 1:length(A)
+        x = get_one_parallel_database(A, i)
+        push!(parallel_databases, x)
+    end
+    
+    return parallel_databases
+end
