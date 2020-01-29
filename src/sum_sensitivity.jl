c0b709ac88d82d2a72ca374ec0148abc67e13100
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-01-29 08:04:54 +0530

    Update sum_sensitivity.jl

--- src/sum_sensitivity.jl
+++ src/sum_sensitivity.jl
@@ -4,21 +4,16 @@ include("get_one_parallel_database.jl")
 include("get_multiple_parallel_databases.jl")
 
 
-function query(A)
-    return sum(A)
-end
-
-
-function sensitivity(A, query)
+function sensitivity(A)
     
     db = A
     pdbs = get_multiple_parallel_databases(A)
     
-    full_db_result = query(db)
+    full_db_result = sum(db)
     
     max_distance = 0
     for pdb in pdbs
-        pdb_result = query(pdb)
+        pdb_result = sum(pdb)
     
         db_distance = abs(pdb_result - full_db_result)
     
