4fc889df82592fdd7d43e26f115bdb5dd6034b53
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-01-29 15:31:46 +0530

    Delete get_one_parallel_database.jl

--- src/get_one_parallel_database.jl
+++
@@ -1,9 +0,0 @@
-module DiffPrivacy
-
-function get_one_parallel_database(A, remove_index)
-    
-    # Returns an array A, without the element at position remove_index, of size (1, length(A) - 1)
-    
-    
-    return vcat(A[1:remove_index-1], A[remove_index+1:length(A)])'
-end
