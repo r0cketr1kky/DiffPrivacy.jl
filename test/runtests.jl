1db3ea8f702e7a63b60fd0ce40276e504bfb50a6
Rakshit Naidu <rakshit.naidu07@gmail.com>
2020-01-29 15:53:17 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,28 @@
 using DiffPrivacy, Test
 
-@test begin
-  
-  @info "Testing one parallel database"
-  x = [1,2,3,4,5,6,7,8,9]
-  y = DiffPrivacy.get_one_parallel_database(x, 2)
-  @test y == [1 3 4 5 6 7 8 9]
+
+@info "Testing one parallel database"
+x = [1,2,3,4,5,6,7,8,9]
+y = DiffPrivacy.get_one_parallel_database(x, 2)
+@test y == [1 3 4 5 6 7 8 9]
 
 
-  @info "Testing multiple parallel databases"
-  x = [1,2,3,4]
-  y = DiffPrivacy.get_multiple_parallel_databases(x)
-  @test y == [[2 3 4],[1 3 4],[1 2 4],[1 2 3]]
+@info "Testing multiple parallel databases"
+x = [1,2,3,4]
+y = DiffPrivacy.get_multiple_parallel_databases(x)
+@test y == [[2 3 4],[1 3 4],[1 2 4],[1 2 3]]
 
 
-  @info "Testing sum sensitivity"
-  x = [1,2,3,4]
-  y = DiffPrivacy.sum_sensitivity(x)
-  @test y == 4
+@info "Testing sum sensitivity"
+x = [1,2,3,4]
+y = DiffPrivacy.sum_sensitivity(x)
+@test y == 4
 
 
-  @info "Testing mean sensitivity"
-  x = [3,6,9,12]
-  y = DiffPrivacy.mean_sensitivity(x)
-  @test y == 1.5
+@info "Testing mean sensitivity"
+x = [3,6,9,12]
+y = DiffPrivacy.mean_sensitivity(x)
+@test y == 1.5
 
 
-  @info "All tests completed"
-  
-end
-  
+@info "All tests completed"
